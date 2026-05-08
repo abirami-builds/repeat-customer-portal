de2274b798171f0c296cc3a7ffb5b51272b5a2aa
Abirami2709 <babirami448@gmail.com>
2026-05-08 19:45:40 +0530

    Initial FastAPI backend setup

---
+++ main.py
@@ -0,0 +1,65 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Database setup
+DATABASE_URL = "sqlite:///./orders.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# Table
+class Order(Base):
+    __tablename__ = "orders"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    customer_name = Column(String)
+    item = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+    return {"message": "API is working with DB"}
+
+@app.post("/add-order")
+def add_order(customer_name: str, item: str):
+    db = SessionLocal()
+    new_order = Order(customer_name=customer_name, item=item)
+    db.add(new_order)
+    db.commit()
+    db.close()
+    return {"message": "Order saved in database"}
+
+@app.get("/orders")
+def get_orders():
+    db = SessionLocal()
+    orders = db.query(Order).all()
+    db.close()
+    return orders
+
+@app.get("/repeat-customers")
+def repeat_customers():
+    db = SessionLocal()
+    orders = db.query(Order).all()
+    
+    count = {}
+    for order in orders:
+        name = order.customer_name
+        count[name] = count.get(name, 0) + 1
+    
+    repeat = [name for name, c in count.items() if c > 1]
+    
+    db.close()
+    return {"repeat_customers": repeat}
